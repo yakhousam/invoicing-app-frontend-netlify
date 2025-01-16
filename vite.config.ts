bf1288dcbeae9d0b34dac9a10b8830223cea4d94
yakhousam <ykhoudir@gmail.com>
2025-01-16 14:18:18 +0100

    feat: set public directory in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
+  publicDir: "public", // The directory to serve as the public directory
   resolve: {
     alias: {
       "@": "/src",
