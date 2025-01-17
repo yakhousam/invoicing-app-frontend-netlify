a0bae1c271e4b52f128da4a10506226a6fd07c45
yakhousam <ykhoudir@gmail.com>
2025-01-17 11:09:13 +0100

    refactor: remove  and update config to use dynamic URLs

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,13 @@ export const domain = import.meta.env.VITE_APP_COGNITO_DOMAIN;
 export const region = import.meta.env.VITE_APP_AWS_REGION;
 export const clientId = import.meta.env.VITE_APP_COGNITO_CLIENT_ID;
 export const identityPoolId = import.meta.env.VITE_APP_COGNITO_IDENTITY_POOL_ID;
-export const redirectUri = import.meta.env.VITE_APP_COGNITO_REDIRECT_URI;
-export const singoutUri = import.meta.env.VITE_APP_COGNITO_SIGN_OUT_URI;
 export const responeType = import.meta.env.VITE_APP_COGNITO_RESPONSE_TYPE;
 export const scope = import.meta.env.VITE_APP_COGNITO_SCOPE;
 export const bucket = import.meta.env.VITE_APP_S3_BUCKET_NAME;
 export const idpEndpoint = import.meta.env.VITE_APP_COGNITO_IDP_ENDPOINT;
+
+export const redirectUri = `${window.location.origin}/overview`;
+export const singoutUri = window.location.origin;
 
 // API
 export const invoicesUrl = import.meta.env.VITE_APP_INVOICES_URL;
