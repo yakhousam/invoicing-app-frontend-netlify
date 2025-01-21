728274b0bcf753f5da9545be94eaf802db57b0e1
yakhousam <ykhoudir@gmail.com>
2025-01-21 14:49:14 +0100

    debug again

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -7,17 +7,12 @@ const useLogout = () => {
   const handleLogout = () => {
     // auth.removeUser();
     auth.signoutSilent();
-    const clientId1 = config.clientId;
-    const logoutUri1 = config.singoutUri;
-    const cognitoDomain1 = config.domain;
-    console.log("clientId1", clientId1);
-    console.log("logoutUri1", logoutUri1);
-    console.log("cognitoDomain1", cognitoDomain1);
-    // window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-    const clientId = "17pae71c1gerpu90vu2uieo6o8";
-    const logoutUri = "https://invoicing-app.khoudiryaya.dev";
-    const cognitoDomain =
-      "https://invoicing-app-v2-541303084246.auth.us-east-1.amazoncognito.com";
+    const clientId = config.clientId;
+    const logoutUri = config.singoutUri;
+    const cognitoDomain = config.domain;
+    console.log("logoutUri", logoutUri);
+    console.log("cognitoDomain", cognitoDomain);
+    console.log("clientId", clientId);
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
   return handleLogout;
