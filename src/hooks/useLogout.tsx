5870e3e684392b2227dacc3b12a36ef2717ba943
yakhousam <ykhoudir@gmail.com>
2025-01-21 14:52:33 +0100

    clear console log

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -5,14 +5,10 @@ const useLogout = () => {
   const auth = useAuth();
 
   const handleLogout = () => {
-    // auth.removeUser();
     auth.signoutSilent();
     const clientId = config.clientId;
     const logoutUri = config.singoutUri;
     const cognitoDomain = config.domain;
-    console.log("logoutUri", logoutUri);
-    console.log("cognitoDomain", cognitoDomain);
-    console.log("clientId", clientId);
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
   return handleLogout;
