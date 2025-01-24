e80e62a1ed1054cfb4e6ff26c808ac520943a4fe
yakhousam <ykhoudir@gmail.com>
2025-01-24 19:17:56 +0100

    add global setup for Playwright tests with Cognito authentication

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+dotenv.config();
+
+const baseURL = "http://localhost:3001";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  globalSetup: "./playwright/global-setup.ts",
+  testDir: "./e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,32 +24,45 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    storageState: "playwright/.auth/storage.json",
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
+  testMatch: "**/*.spec.ts", // Adjust based on your test naming pattern
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: {
+    //     ...devices["Desktop Firefox"],
+    //     // Use prepared auth state.
+    //     storageState: "playwright/.auth/user.json",
+    //   },
+    //   dependencies: ["setup"],
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: {
+    //     ...devices["Desktop Safari"],
+    //     storageState: "playwright/.auth/user.json",
+    //   },
+    //   dependencies: ["setup"],
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -71,9 +86,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev",
+    url: baseURL,
+    reuseExistingServer: !process.env.CI,
+  },
 });
---
+++ playwright/global-setup.ts
@@ -0,0 +1,102 @@
+import {
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserPool,
+} from "amazon-cognito-identity-js";
+
+import { chromium, FullConfig } from "@playwright/test";
+
+const authKey = `oidc.user:${process.env.VITE_APP_COGNITO_AUTHORITY}:${process.env.VITE_APP_COGNITO_CLIENT_ID}`;
+
+// Autheticate once and save the session state to a file. the tests will use this file to authenticate
+// This is useful when you want to run the tests multiple times without having to authenticate each time
+async function globalSetup(config: FullConfig) {
+  const browser = await chromium.launch();
+  const { baseURL, storageState } = config.projects[0].use;
+  const context = await browser.newContext({
+    storageState: undefined,
+  });
+  const page = await context.newPage();
+
+  // Authenticate with Cognito
+  const authValue = await cognitoAuthenticate();
+
+  await page.goto(baseURL!);
+  await page.evaluate(
+    ({ authKey, authValue }) => {
+      localStorage.setItem(authKey, JSON.stringify(authValue));
+    },
+    { authKey, authValue }
+  );
+  // Save storage state
+  await context.storageState({ path: storageState as string });
+
+  await browser.close();
+}
+
+export default globalSetup;
+
+const TEST_USER = {
+  EMAIL: process.env.COGNITO_TEST_EMAIL!,
+  PASSWORD: process.env.COGNITO_TEST_PASSWORD!,
+};
+
+const poolData = {
+  UserPoolId: process.env.VITE_APP_USER_POOL_ID!,
+  ClientId: process.env.VITE_APP_COGNITO_CLIENT_ID!,
+};
+
+const userPool = new CognitoUserPool(poolData);
+
+type AuthStorageValue = ReturnType<typeof transformCognitoSession>;
+
+async function cognitoAuthenticate() {
+  const authenticationDetails = new AuthenticationDetails({
+    Username: TEST_USER.EMAIL,
+    Password: TEST_USER.PASSWORD,
+  });
+
+  const userData = {
+    Username: TEST_USER.EMAIL,
+    Pool: userPool,
+  };
+
+  const cognitoUser = new CognitoUser(userData);
+
+  return new Promise<AuthStorageValue>((resolve, reject) => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: async (result) => {
+        const authValue = transformCognitoSession(result);
+        resolve(authValue);
+      },
+      onFailure: (err) => {
+        reject(err);
+      },
+    });
+  });
+}
+
+function transformCognitoSession(session: Record<string, any>) {
+  return {
+    id_token: session.idToken.jwtToken,
+    session_state: null,
+    access_token: session.accessToken.jwtToken,
+    refresh_token: session.refreshToken.token,
+    token_type: "Bearer",
+    scope: session.accessToken.payload.scope,
+    profile: {
+      sub: session.idToken.payload.sub,
+      iss: session.idToken.payload.iss,
+      "cognito:username": session.idToken.payload["cognito:username"],
+      origin_jti: session.idToken.payload.origin_jti,
+      aud: session.idToken.payload.aud,
+      event_id: session.idToken.payload.event_id,
+      token_use: session.idToken.payload.token_use,
+      name: session.idToken.payload.name,
+      exp: session.idToken.payload.exp,
+      iat: session.idToken.payload.iat,
+      email: session.idToken.payload.email,
+    },
+    expires_at: session.idToken.payload.exp,
+  };
+}
